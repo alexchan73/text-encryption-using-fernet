cd22b840d4a0e39722ce4660e29b483f1306c42d
Alex <65793940+alexchan73@users.noreply.github.com>
2022-10-04 13:31:19 -0700

    Update fernet_encrypt.py

--- fernet_encrypt.py
+++ fernet_encrypt.py
@@ -12,13 +12,13 @@ def generate_key():
     key_var  = Fernet(key)
     return key_var
 
-
 def encrypt(usr_input, key_var):
     encry_token = key_var.encrypt(usr_input)
     return(encry_token)
 
 def decrypt(key_var, encry_token):
     return key_var.decrypt(encry_token)
+
 #when an output has b in front, it is in byte format, to get rid of it -> .decode()
 def main():
     usr_input = input("Please enter a message you want to encrypt: ").encode() #converting to byte format
